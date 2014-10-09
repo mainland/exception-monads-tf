083407a39c96a2d7ec27318a462e611cf874db23
Geoffrey Mainland <mainland@apeiron.net>
2014-10-09 14:17:30 -0400

    Update for compatibility with transformers >= 0.4.

--- Control/Monad/Exception/Instances.hs
+++ Control/Monad/Exception/Instances.hs
@@ -1,14 +1,14 @@
--- |
--- Module      :  Control.Monad.Exception.Instances
--- Copyright   :  (c) Harvard University 2008-2010
---             :  (c) Geoffrey Mainland 2011-2012
--- License     :  BSD-style
--- Maintainer  :  mainland@eecs.harvard.edu
-
-{-# OPTIONS_GHC -fno-warn-orphans #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+-- |
+-- Module      :  Control.Monad.Exception.Instances
+-- Copyright   :  (c) Harvard University 2008-2010
+--             :  (c) Geoffrey Mainland 2011-2014
+-- License     :  BSD-style
+-- Maintainer  :  mainland@eecs.harvard.edu
 
 module Control.Monad.Exception.Instances where
 
